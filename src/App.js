983b9b0f22ee1b130900f900a51671afcc54e970
unknown <emon.me44@gmail.com>
2022-09-29 23:14:44 +0600

    final

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Display from './Component/Display/Display';
 import Navber from './Component/Navber/Navber';
+import Blog from './Component/blog/Blog'
 
 function App() {
   return (
     <div>
       <Navber></Navber>
       <Display></Display>
+      <Blog></Blog>
     </div>
   );
 }

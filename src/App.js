c7a3a5babc3f31c2f3881f852ad12b8599c09442
unknown <emon.me44@gmail.com>
2022-09-29 08:20:10 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-// import './App.css';
-
-import { useEffect, useState } from "react";
-import TheData from "./compo/TheData/TheData";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-  const [name, setName] = useState([])
-
-  useEffect(() => {
-    fetch('fakeDB.json')
-      .then(res => res.json())
-      .then(data => setName(data))
-  }, [])
-
   return (
     <div className="App">
-      {
-        name.map(nm => <TheData key={nm.id} data={nm}></TheData>)
-      }
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
